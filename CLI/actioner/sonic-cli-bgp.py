d849c9048d13f11ab4e46bf47dcc830195e2f492
Alberto Martin Martin <alberto.martin@htecgroup.com>
2025-11-05 11:34:07 +0100

    Feature/nosi 155 create remove bgp router (#1)
    * NOSI-155: First approach to implement router bgp.
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Added blank line at end of the bgp.xml
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Added bgp_global_show.j2 to render the
    output of show bgp global command.
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Modified bgp.xml to use clish_pyobj
    action for BGP commands instead of direct python3
    calls.
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Fixed call_pyobj to load Python 3 lib.
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Fixed sonic_cli_mclag by using Python3
    print statements.
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Added 'show bgp-global-config' command
    to display BGP global configuration
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    * NOSI-155: Modified sonic-cli-bgp to check if bgp
    already exists with the same AS number, in which
    case it returns an error.
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>
    
    ---------
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>

---
+++ CLI/actioner/sonic-cli-bgp.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+
+import sys
+from cli_client import ApiClient, Path
+from rpipe_utils import pipestr
+from scripts.render_cli import show_cli_output
+
+
+def bgp_global_path():
+    """Returns the base path for BGP global configuration"""
+    return Path("/restconf/data/openconfig-bgp:bgp/global")
+
+
+def bgp_global_config_path():
+    """Returns the path for BGP global config container"""
+    return Path("/restconf/data/openconfig-bgp:bgp/global/config")
+
+
+def check_ok(resp):
+    """Check if response is OK and print error if not"""
+    if not resp.ok():
+        print(resp.error_message())
+        return 1
+    return 0
+
+
+def render(path, template):
+    """Get data from API and render using template"""
+    resp = get_openconfig_bgp_bgp_global(path)
+    if not resp.ok():
+        print(resp.error_message())
+        return 1
+    if resp.content:
+        show_cli_output(template, resp.content)
+    return 0
+
+
+def get_openconfig_bgp_bgp_global(path):
+    return ApiClient().get(path, ignore404=True)
+
+class Handlers:
+    @staticmethod
+    def show_openconfig_bgp_bgp_global_config(template, *args):
+        """Get BGP global configuration"""
+        return render(bgp_global_path(), template)
+
+    @staticmethod
+    def post_openconfig_bgp_bgp_global(as_number, router_id):
+        """Create BGP global configuration using POST"""
+        body = {
+            "openconfig-bgp:config": {
+                "as": int(as_number),
+                "router-id": router_id
+            }
+        }
+        resp = ApiClient().post(bgp_global_path(), body)
+        if not resp.ok():
+            if "configuration already exists" in resp.error_message():
+                resp = get_openconfig_bgp_bgp_global(bgp_global_path())
+                bgp_asn = resp.content['openconfig-bgp:global']['config']['as']
+                if bgp_asn == int(as_number):
+                    return 0
+                print(f"BGP instance is already running; AS is {bgp_asn}")
+                print(f"Remove existing configuration ('no router bgp {bgp_asn}') before adding a new one.")
+            else:
+                print(resp.error_message())
+            return 1
+        return 0
+
+    @staticmethod
+    def patch_openconfig_bgp_bgp_global_config(as_number, router_id):
+        """Update BGP global configuration using PATCH"""
+        body = {
+            "openconfig-bgp:config": {
+                "as": int(as_number),
+                "router-id": router_id
+            }
+        }
+        resp = ApiClient().patch(bgp_global_config_path(), body)
+        return check_ok(resp)
+
+    @staticmethod
+    def put_openconfig_bgp_bgp_global_config(as_number, router_id):
+        """Replace BGP global configuration using PUT"""
+        body = {
+            "openconfig-bgp:config": {
+                "as": int(as_number),
+                "router-id": router_id
+            }
+        }
+        resp = ApiClient().put(bgp_global_config_path(), body)
+        return check_ok(resp)
+
+    @staticmethod
+    def delete_openconfig_bgp_bgp_global():
+        """Delete BGP global configuration"""
+        resp = ApiClient().delete(bgp_global_path())
+        return check_ok(resp)
+
+
+def run(func, args):
+    """Execute the specified handler function"""
+    return getattr(Handlers, func)(*args)
+
+
+if __name__ == '__main__':
+    pipestr().write(sys.argv)
+    func = sys.argv[1]
+    run(func, sys.argv[2:])
